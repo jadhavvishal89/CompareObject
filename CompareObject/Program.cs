5f628b95faa25322381e6a55e7647952d1211b53
Vishal <jadhavvishal89@outlook.com>
2023-03-19 15:50:41 +0530

    Add project files.

---
+++ CompareObject/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CompareObject;
+
+Console.WriteLine("Welcon to Object equale check!\n");
+Students student1 = new Students { studentid = 1, name = "vishal" };
+Students student2 = new Students { studentid = 1, name = "vishal" };
+Console.WriteLine("Compare student1 and student2 object\n");
+Console.WriteLine("student1 and student2 object ="+ student1.Equals(student2) +"\n");
+
+Students student3 = new Students { studentid = 2, name = "vishal2" };
+Console.WriteLine("student1 and student3 object =" + student1.Equals(student3));
---
+++ CompareObject/Students.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompareObject
+{
+    public class Students : IEquatable<Students>
+    {
+        public int studentid;
+        public string name;
+
+        public bool Equals(Students? other)
+        {
+            if (ReferenceEquals(other,null))
+            {
+                return false;
+            }
+            if (ReferenceEquals(other,this)) { return true; }
+
+            return studentid.Equals(other.studentid) && name.Equals(other.name);
+        }
+    }
+}
